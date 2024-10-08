dd03295109d5b69ca17207e7d88c17f15e793b65
rrydey <145787082+rrydey@users.noreply.github.com>
2024-10-08 18:48:54 +0500

    update

--- script.js
+++ script.js
@@ -147,3 +147,21 @@ document.addEventListener("DOMContentLoaded", () => {
     applyFilter(category);
   });
 });
+
+
+//Course Page Accordion
+
+var acc = document.getElementsByClassName("course-page-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
